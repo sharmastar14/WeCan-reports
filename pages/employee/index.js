700c33a97277083a625402a6dc32d1218ced93b4
Anjani Sharma <sharmastaranjani@gmail.com>
2022-07-24 14:34:40 +0545

    added submit handler

--- pages/employee/index.js
+++ pages/employee/index.js
@@ -1,1 +1,6 @@
 console.log("hello world");
+let addEmployeeForm = document.getElementById("add-employee-form");
+addEmployeeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("submitted");
+});
