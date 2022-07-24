909132eb2d97765aefaea5775047cf0307335bfd
Anjani Sharma <sharmastaranjani@gmail.com>
2022-07-24 16:06:52 +0545

    fixed list item

--- pages/employee/index.js
+++ pages/employee/index.js
@@ -1,34 +1,64 @@
 const employeeListUL = document.getElementById("employee-list");
-const employees = JSON.parse(localStorage.getItem("employees"));
+const employees = JSON.parse(localStorage.getItem("employees")) ?? [];
 const addEmployeeForm = document.getElementById("add-employee-form");
 
 employees.forEach((employee) => {
-  const li = document.createElement("li");
-  const anchor = document.createElement("a");
-  anchor.classList.add(
+  const employeeListItem = document.createElement("li");
+  employeeListItem.classList.add(
     "list-group-item",
     "list-group-item-action",
     "d-flex",
     "justify-content-between",
     "align-items-center"
   );
-  const button = document.createElement("button");
+  const addClassBtn = document.createElement("button");
   const badge = document.createElement("span");
-  badge.classList.add("badge", "badge-light");
+  badge.classList.add("badge", "badge-warning", "badge-pill", "mr-1");
   badge.textContent = "9";
-  button.setAttribute("data-toggle", "modal");
-  button.setAttribute("data-target", "#exampleModal");
-  button.classList.add("btn", "btn-secondary");
-  button.textContent = "Add class +";
+  addClassBtn.setAttribute("data-toggle", "modal");
+  addClassBtn.setAttribute("data-target", "#exampleModal");
+  addClassBtn.classList.add("btn", "btn-outline-primary", "btn-sm");
+  addClassBtn.textContent = "Add class +";
   const div = document.createElement("div");
-  anchor.textContent = employee.name;
-  anchor.appendChild(button);
+  employeeListItem.textContent = employee.name;
+  employeeListItem.appendChild(addClassBtn);
   div.appendChild(badge);
-  div.appendChild(anchor);
-  li.appendChild(div);
-  employeeListUL.appendChild(li);
+  div.appendChild(addClassBtn);
+  employeeListItem.appendChild(div);
+  employeeListUL.appendChild(employeeListItem);
 });
 addEmployeeForm.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("submitted");
 });
+
+/*
+employees = [
+  { id: 1, name: "Suman", phone: "23132131231" },
+  { id: 2, name: "Anjani", phone: "321321111" },
+];
+
+const classes = [
+  {
+    employeeId: 2,
+    school: "AVM",
+    name: "Class 6, Section A",
+    type: "single",
+    date: "2020-02-20",
+  },
+  {
+    employeeId: 1,
+    school: "Floroscent",
+    name: "Class 5, Section A",
+    type: "dual",
+    date: "2020-02-20",
+  },
+  {
+    employeeId: 1,
+    school: "Floroscent",
+    name: "Class 5, Section B",
+    type: "single",
+    date: "2020-02-20",
+  },
+];
+*/
