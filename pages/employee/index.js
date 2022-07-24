21294d086750a13c5b4939b08b6f8deace866322
suman maharjan <suman.maharjan@joinsage.com>
2022-07-24 15:33:09 +0545

    wip getting employee list items done

--- pages/employee/index.js
+++ pages/employee/index.js
@@ -1,5 +1,33 @@
-console.log("hello world");
-let addEmployeeForm = document.getElementById("add-employee-form");
+const employeeListUL = document.getElementById("employee-list");
+const employees = JSON.parse(localStorage.getItem("employees"));
+const addEmployeeForm = document.getElementById("add-employee-form");
+
+employees.forEach((employee) => {
+  const li = document.createElement("li");
+  const anchor = document.createElement("a");
+  anchor.classList.add(
+    "list-group-item",
+    "list-group-item-action",
+    "d-flex",
+    "justify-content-between",
+    "align-items-center"
+  );
+  const button = document.createElement("button");
+  const badge = document.createElement("span");
+  badge.classList.add("badge", "badge-light");
+  badge.textContent = "9";
+  button.setAttribute("data-toggle", "modal");
+  button.setAttribute("data-target", "#exampleModal");
+  button.classList.add("btn", "btn-secondary");
+  button.textContent = "Add class +";
+  const div = document.createElement("div");
+  anchor.textContent = employee.name;
+  anchor.appendChild(button);
+  div.appendChild(badge);
+  div.appendChild(anchor);
+  li.appendChild(div);
+  employeeListUL.appendChild(li);
+});
 addEmployeeForm.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("submitted");
--- scripts.js
+++ scripts.js
@@ -3,3 +3,8 @@
 //git commit -m "your commit message your work"
 //git push origin master
 console.log("hello");
+// const employees = [
+//   { name: "Suman", phone: null },
+//   { name: "Anjani", phone: "9841707850" },
+// ];
+// localStorage.setItem("employees", JSON.stringify(employees));
