14499ff7f4bb1f11cf89c4d6c4f9f34f8ba9b254
suman maharjan <suman.maharjan@joinsage.com>
2022-07-27 20:00:37 +0545

    added class object in local storge and wip link

--- pages/employee/index.js
+++ pages/employee/index.js
@@ -3,29 +3,34 @@ function getFormValueFromEvent(e, fieldname) {
 }
 
 const employeeListUL = document.getElementById("employee-list");
-const employees = JSON.parse(localStorage.getItem("employees")) ?? [];
 const addEmployeeForm = document.getElementById("add-employee-form");
 const employeeDetail = document.getElementById("employee-detail");
 const addClassForm = document.getElementById("add-class-form");
+
+//database
+const employees = JSON.parse(localStorage.getItem("employees")) ?? [];
+const classes = JSON.parse(localStorage.getItem("classes")) ?? [];
 
 function DisplayEmployees() {
   employeeListUL.innerHTML = "";
   const params = new URLSearchParams(window.location.search);
   employeeDetail.textContent = params.get("id");
-  console.log(params.get("id"));
   employees.forEach((employee) => {
     const isSelected = params.get("id") == employee.id;
-    console.log(isSelected);
     const employeeListItem = document.createElement("li");
+    const linkElement = document.createElement("a");
+    linkElement.classList.add(
+      "d-flex",
+      "justify-content-between",
+      "align-items-center"
+    );
     employeeListItem.classList.add(
       "list-group-item",
       "list-group-item-action",
-      "d-flex",
-      "justify-content-between",
-      "align-items-center",
       isSelected ? "active" : undefined
     );
-
+    const link = window.location.href + "?id=" + employee.id;
+    linkElement.href = link;
     const addClassBtn = document.createElement("button");
     const badge = document.createElement("span");
     badge.classList.add("badge", "badge-warning", "badge-pill", "mr-1");
@@ -45,11 +50,12 @@ function DisplayEmployees() {
     });
 
     const div = document.createElement("div");
-    employeeListItem.textContent = employee.name;
-    employeeListItem.appendChild(addClassBtn);
+    linkElement.textContent = employee.name;
+    linkElement.appendChild(addClassBtn);
     div.appendChild(badge);
     div.appendChild(addClassBtn);
-    employeeListItem.appendChild(div);
+    linkElement.appendChild(div);
+    employeeListItem.appendChild(linkElement);
     employeeListUL.appendChild(employeeListItem);
   });
 }
@@ -70,17 +76,17 @@ addEmployeeForm.addEventListener("submit", (e) => {
 
   DisplayEmployees();
 });
-
 addClassForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("daily updates submitted");
-
   const classObj = {
     employeeId: getFormValueFromEvent(e, "id"),
     schoolName: getFormValueFromEvent(e, "schoolName"),
     className: getFormValueFromEvent(e, "className"),
     classType: getFormValueFromEvent(e, "classType"),
   };
+  classes.push(classObj);
+  localStorage.setItem("classes", JSON.stringify(classes));
+
   //reset form so that next time modal opens, I dont see the same values.
   e.target.reset();
 
